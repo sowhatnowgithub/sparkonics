7de83a2d00f6bf3ce128bff48ebda828b0a541d5
sowhatnow_git <forkaliuse6500u@gmail.com>
2025-07-08 11:27:13 +0530

    Forgot ot commit

--- Env.php
+++ Env.php
@@ -5,10 +5,10 @@ namespace Sowhatnow;
 class Env
 {
     public const BASE_PATH = __DIR__;
-    public const HOST_ADDRESS = "https://80b9-103-217-212-95.ngrok-free.app"; // For tesing http://localhost
+    public const HOST_ADDRESS = "http://localhost"; // For tesing http://localhost
     public const API_HOST = "0.0.0.0";
     public const API_PORT = "1978"; // why 1978? Mums Birthday
-    public const API_BASE_URL = "https://80b9-103-217-212-95.ngrok-free.app"; // For testing http://localhost:1978
+    public const API_BASE_URL = "http://localhost:1978"; // For testing http://localhost:1978
     public const API_IMAGES_PATH = __DIR__ . "/api/Models/Database/Images";
     // also the thrds.js for websocker connection
 }
